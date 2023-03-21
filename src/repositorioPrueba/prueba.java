f526b3d97621539f5c27f937d9bb956b3fc03af0
Flor y Guido <Flor y Guido@DESKTOP-FQAQLNG>
2023-03-21 13:03:49 -0300

    Agregué la variable a

---
+++ src/repositorioPrueba/prueba.java
@@ -0,0 +1,15 @@
+package repositorioPrueba;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a= (2+3);
+		
+		System.out.println(a);
+
+	}
+
+}
